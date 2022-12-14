86984b418d6f5ecca47aa5b851bec4378ea2ae9f
jihye kim <74666269+kzh4295@users.noreply.github.com>
2022-12-14 12:53:28 +0900

    Create 약수의합.js

---
+++ 내코드/Lv1/약수의합.js
@@ -0,0 +1,44 @@
+// 약수를 구하기 >>> n값을 나눈 나머지가 0인 숫자를 배열로 떨구기
+// 배열의 합을 구하기 >>> reduce사용해보기
+
+ 
+
+function solution(n){
+    let arr = [];
+    for(let i=1; i <=n; i++){
+        if(n%i ===0){
+            arr.push(i);
+        }
+    }
+    return arr.reduce((acc, ele)=>{ return  acc + ele }, 0);
+}
+
+
+=======================타인의 코드 잘짜셨넹===========================================
+
+function solution(num) {
+    let sum = 0;
+    for (let i = 1; i <= num; i++) {
+        if (num % i === 0) sum += i
+    }
+    return sum
+}
+
+=======================타인의 코드 잘짜셨넹===========================================
+
+function solution(n) {
+    // var answer = 0;
+    let result = 0;
+
+    for(let m = 1 ; m <= n ; m++ ){
+        if(n%m===0){
+           result=result+m 
+        }
+    }
+
+    return result;
+}
+
+---------------------느낀점------------------------------------------------------------
+
+return 해야할 값이 배열이 아니라 숫자이면 굳이 배열을 생성하지 말고 바로 값을 계산해도 됐었겠구나..
