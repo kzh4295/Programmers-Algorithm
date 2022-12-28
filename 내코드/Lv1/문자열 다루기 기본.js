033da740eaa8bd9c6e81888e5b624c7a92028f83
jihye kim <74666269+kzh4295@users.noreply.github.com>
2022-12-29 08:36:00 +0900

    Create 문자열 다루기 기본.js

---
+++ 내코드/Lv1/문자열 다루기 기본.js
@@ -0,0 +1,16 @@
+소요시간 => 11:21
+
+function solution(s) {
+    let answer = true;
+    let total = 0;
+    
+   total +=s% 10 
+   if(!total){
+       return answer = false
+   }
+    
+    return answer;
+}
+
+=======================타인의 코드=============================
+ 내코드가 잘 짠거 
