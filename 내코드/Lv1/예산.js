e5c4802723b9d9d24aabce1394250eafce59593c
jihye kim <74666269+kzh4295@users.noreply.github.com>
2023-02-19 17:45:06 +0900

    Create 예산.js

---
+++ 내코드/Lv1/예산.js
@@ -0,0 +1,21 @@
+function solution(d, budget) {
+    let total = 0 // idx만큼 부서가 신청한 금액의 합
+    let idx = 0;  // idx번째 부서가 신청
+    let tempArr = d.sort((a, b) => a - b) // 오름 차순으로 부서가 신청한 값 변경
+    
+    while(total <= budget){
+        total += tempArr[idx]
+        idx++
+    }
+    return idx-1;
+}
+
+================================타인의 코드====================================================
+function solution(d, budget) {
+    d.sort((a, b) => a - b);
+
+    while (d.reduce((a, b) => (a + b), 0) > budget) d.pop();
+
+    return d.length;
+}
+
