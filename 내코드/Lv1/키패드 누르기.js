48d3480ea3a26ff304e4ddf58cd6629c9e2a54e6
jihye kim <74666269+kzh4295@users.noreply.github.com>
2023-02-23 01:18:05 +0900

    Update 키패드 누르기.js

--- 내코드/Lv1/키패드 누르기.js
+++ 내코드/Lv1/키패드 누르기.js
@@ -1,55 +1,65 @@
-function solution(numbers, hand) {
-    let answer = '';
-    let leftValue = 10                              // leftValue 시작값
-    let rightValue = 12                             // rightValueValue 시작값
-    
-    let zeroToEleven = numbers.map((ele)=>{
-        return ele = (ele === 0) ? 11 : ele;        //numbers의 0의 값을 11로 변경하여 distance와의 값을 계산하고자함
-    })
-    
-    
-    zeroToEleven.forEach((ele, idx)=>{
-        let distanceLeft = 0;                       //left - ele 사이의 거리
-        let distanceRight = 0;                      // right - ele 사이의 거리
-        
-        switch(ele){
-            case 1: case 4: case 7:
-                zeroToEleven[idx] = 'L';
-                leftValue = ele;
-                break;
-            case 3: case 6: case 9:
-                zeroToEleven[idx] = 'R';
-                rightValue = ele;
-                break;
-                
-                
-            case 2: case 5: case 8: case 11:
-                distanceLeft = Math.abs(Math.trunc((leftValue - ele)/3)) + Math.abs((leftValue - ele)%3)
-                distanceRight = Math.abs(Math.trunc((rightValue - ele)/3)) + Math.abs((rightValue - ele)%3)
-                
-                if(distanceLeft > distanceRight){
-                    zeroToEleven[idx] = 'R'
-                    rightValue = ele;
-                } else if(distanceLeft < distanceRight){
-                    zeroToEleven[idx] = 'L'
-                    leftValue = ele;
-                } else if(distanceLeft === distanceRight){
-                    if(hand === 'left'){
-                        zeroToEleven[idx] = 'L'
-                        leftValue = ele;
-                    } else {
-                        zeroToEleven[idx] = 'R'
-                        rightValue = ele;
-                    }
-                }
-            
-        }
-    })
-    
-    answer = zeroToEleven.join('')
-    return answer;
-}
+let leftValue = 10; // leftValue 초기값 (* => 10)
+let rightValue = 12; // rightValueValue 초기값 (# => 12)
 
+function solution(numbers, hand) {
+  let answer = ''; //solution함수를 return 하고자 하는 값
+
+  const elevenNumber = numbers.map((number) => {
+    return number || 11;
+  }); // 매개변수로 받는 numbers의 요소 중, number가 0인 값을 11로 변환하여 만든 새로운 배열을 return하는 함수
+
+  const checkDistance = (param) => {
+    // 거리 체크하는 함수
+    // 의도 => 행의 이동 거리 + 열의 이동 거리
+    // 행의 이동거리 => leftValue 혹은 rightValue와 매개변수(누른 키패드 값)의 차이에 3을 나눈 몫만큼 이동
+    // 열의 이동거리 => leftValue 혹은 rightValue와 매개변수(누른 키패드 값)의 차이에 3을 나눈 나머지만큼 이동
+
+    let distanceLeft = Math.abs(Math.trunc((leftValue - param) / 3)) + Math.abs((leftValue - param) % 3);
+    let distanceRight = Math.abs(Math.trunc((rightValue - param) / 3)) + Math.abs((rightValue - param) % 3);
+
+    return distanceLeft - distanceRight; // 양수 : distanceLeft가 머니까 R // 음수 : distanceRight가 머니까 L // 0 : 거리가 같으므로 손잡이에 따름
+  };
+
+  const checkHand = (param, index, finger) => {
+    // L인지 R인지 확인하는 함수
+    if (finger === 'L') {
+      elevenNumber[index] = finger;
+      leftValue = param;
+    } else {
+      elevenNumber[index] = finger;
+      rightValue = param;
+    }
+  };
+
+  answer = elevenNumber.map((ele, idx) => {
+    switch (ele) {
+      case 1:
+      case 4:
+      case 7:
+        checkHand(ele, idx, 'L');
+        break;
+
+      case 3:
+      case 6:
+      case 9:
+        checkHand(ele, idx, 'R');
+        break;
+
+      case 2:
+      case 5:
+      case 8:
+      case 11:
+        checkDistance(ele) === 0
+          ? checkHand(ele, idx, hand[0].toUpperCase())
+          : checkDistance(ele) > 0
+          ? checkHand(ele, idx, 'R')
+          : checkHand(ele, idx, 'L');
+    }
+  });
+
+  answer = elevenNumber.join('');
+  return answer;
+}
 
 
 =======================================나라님 코드=============================================================
