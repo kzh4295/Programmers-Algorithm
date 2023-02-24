8d36b3944dc144304df4e60259fd14ebf183dd66
문경태(kyungtae Moon) <demoon2016@ncsoft.com>
2023-02-24 09:36:41 +0900

    review

--- Lv1/keypad.js
+++ Lv1/keypad.js
@@ -5,7 +5,8 @@ let leftValue = 10; // leftValue 초기값 (* => 10)
 let rightValue = 12; // rightValueValue 초기값 (# => 12)
 
 function solution(numbers, hand) {
-	let answer = ''; //solution함수를 return 하고자 하는 값
+	// answer 라는 이름보다 result 와 같은 변수 이름이 좋아요.
+	let answer = '';
 
 	const elevenNumber = numbers.map((number) => {
 		return number || 11;
@@ -18,22 +19,14 @@ function solution(numbers, hand) {
 	//
 	// const numberList = numbers.map(convertZeroToEleven);
 
-
-
 	const checkDistance = (param) => {
-		// 거리 체크하는 함수
-		// 의도 => 행의 이동 거리 + 열의 이동 거리
-		// 행의 이동거리 => leftValue 혹은 rightValue와 매개변수(누른 키패드 값)의 차이에 3을 나눈 몫만큼 이동
-		// 열의 이동거리 => leftValue 혹은 rightValue와 매개변수(누른 키패드 값)의 차이에 3을 나눈 나머지만큼 이동
-
 		let distanceLeft = Math.abs(Math.trunc((leftValue - param) / 3)) + Math.abs((leftValue - param) % 3);
 		let distanceRight = Math.abs(Math.trunc((rightValue - param) / 3)) + Math.abs((rightValue - param) % 3);
 
-		return distanceLeft - distanceRight; // 양수 : distanceLeft가 머니까 R // 음수 : distanceRight가 머니까 L // 0 : 거리가 같으므로 손잡이에 따름
+		return distanceLeft - distanceRight;
 	};
 
 	const checkHand = (param, index, finger) => {
-		// L인지 R인지 확인하는 함수
 		if (finger === 'L') {
 			elevenNumber[index] = finger;
 			leftValue = param;
@@ -44,7 +37,14 @@ function solution(numbers, hand) {
 		}
 	};
 
+	// map의 리턴 값이 없어요.
+	// answer = 는 필요 없는 코드에요.
 	answer = elevenNumber.map((ele, idx) => {
+		//switch 문 보다는 if를 써보세요.
+		// if(/1|4|7/.test(ele)){
+		// 	checkHand(ele, idx, 'L');
+		// }
+
 		switch (ele) {
 			case 1:
 			case 4:
@@ -71,5 +71,6 @@ function solution(numbers, hand) {
 	});
 
 	answer = elevenNumber.join('');
+
 	return answer;
 }
