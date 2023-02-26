b5db8e35e971de54fe5e2fe954e970ba39d10d11
김지혜 <kzh4295@naver.com>
2023-02-27 06:08:34 +0900

    집에서 작업 중 업로드

--- Lv1/test/failureRate.js
+++ Lv1/test/failureRate.js
@@ -26,15 +26,35 @@ function solution(N, stages) {
     // console.log(1/8, 3/7, 2/4, 1/2, 0/1 )
 
 
-    for(let i = 1; i<failPerMap.length +1; i++){
-        let max = 0;
-        if(max < failPerMap.get(i)){
-            max = failPerMap.get(i)
-            result.push(max)
-        } 
-        result.push(max)
+    // let max = 0;
+    // for(let i = 0; failPerMap.size; i++){
+    //     if(failPerMap.get(i)>max){
+    //         result.push(failPerMap.get(i));
+    //         max = failPerMap[i]
+    //     } else if(failPerMap.get(i) === max){
+    //         result.push
+    //     }
+    // }
+
+    console.log(failPerMap)
+    for(const [key, value] of failPerMap){
+        console.log(Math.max(value));
     }
-    
+
+    // failPerMap.forEach(function(value, key){
+    //     console.log(key)
+    //     console.log('key:'+ key)
+    //     // console.log('value:'+value)
+        
+    // })
+
+    // const tt = Array.from(failPerMap.entries());
+    // console.log(tt)
+    // tt.reduce((acc, ele, idx)=>{
+    //     console.log(Math.max(tt[0][]))
+    //     console.log(Math.max(ele[0]))
+    // },[])
+
 
 
     console.log(result)
