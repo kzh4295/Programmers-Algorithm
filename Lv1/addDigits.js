1f68a8a77ac3900d836952196234ecd3b009f0e9
jihye kim <74666269+kzh4295@users.noreply.github.com>
2023-02-27 07:49:55 +0900

    Update and rename 자릿수 더하기.js to addDigits.js

---
+++ Lv1/addDigits.js
@@ -0,0 +1,25 @@
+// 자릿수 더하기
+// https://school.programmers.co.kr/learn/courses/30/lessons/12931
+
+//나의 풀이
+function solution(n){
+    var answer = 0;
+    let arr = (n + '').split('')
+    for(let i=0; i<arr.length; i++){
+        answer += Number(arr[i])
+    }
+    
+    return answer;
+}
+
+
+// 타인의 풀이
+function solution(n){
+    return (n+"").split("").reduce((acc, curr) => acc + parseInt(curr), 0)
+
+
+//느낀점
+// - Number()는 생성자함수이자 원시 타입을 변환하는 객체
+// - parseInt() 함수는 문자열 인자를 파싱하여 특정 진수(수의 진법 체계에서 기준이 되는 값)의 정수를 반환합니다.
+// - reduce를 잘 활용해보자
+ 
