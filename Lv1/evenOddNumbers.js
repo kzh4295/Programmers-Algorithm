567ccb1b9239b18b2cba53ba9eb334d5aac8b981
jihye kim <74666269+kzh4295@users.noreply.github.com>
2023-02-27 07:42:46 +0900

    Update and rename 짝수와 홀수.js to evenOddNumbers.js

--- Lv1/evenOddNumbers.js
+++ Lv1/evenOddNumbers.js
@@ -1,3 +1,6 @@
+// 짝수와 홀수
+// https://school.programmers.co.kr/learn/courses/30/lessons/12937
+
 
 //나의 풀이
 function solution(num) {
@@ -32,5 +35,5 @@ function evenOrOdd(num) {
 
 
 //느낀점
-  // - if-else문이 1세트이면 삼항연산자 사용하는 것이 훨씬 보기 좋다
-  // - 0은 짝수가 아니다, 음수에 대한 처리를 하지 않았구나 느꼈다
+  // if-else문이 1세트이면 삼항연산자 사용하는 것이 훨씬 보기 좋다
+  // 0은 짝수가 아니다, 음수에 대한 처리를 하지 않았구나 느꼈다
