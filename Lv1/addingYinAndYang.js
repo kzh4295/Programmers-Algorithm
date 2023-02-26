23a44b8d6b217cbdb320b10be35f13e2fcc5ca66
jihye kim <74666269+kzh4295@users.noreply.github.com>
2023-02-27 08:19:19 +0900

    Update and rename 음양 더하기.js to addingYinAndYang.js

--- Lv1/addingYinAndYang.js
+++ Lv1/addingYinAndYang.js
@@ -1,5 +1,7 @@
-```javascript
-<!-- 나의 풀이 -->
+// 음양 더하기
+// https://school.programmers.co.kr/learn/courses/30/lessons/76501
+
+// 나의 풀이
 function solution(absolutes, signs) {
     let answer = 0;
     let aLen = absolutes.length;
@@ -12,20 +14,14 @@ function solution(absolutes, signs) {
     return answer ;
 }
 
-```
-
-```javascript
-<!-- 타인의 풀이 -->
+// ================================================================
+// 타인의 풀이
 function solution(absolutes, signs) {
 
     return absolutes.reduce((acc, val, i) => acc + (val * (signs[i] ? 1 : -1)), 0);
 }
 
-```
-
-#
-
-//느낀점
-- 배열의 합을 연산할때는 reduce를 생각하자. 그치만 2개의 배열의 루프를 돌릴때는 reduce가 for보다 속도가 느리므로 유의해서 쓰자.
+// 느낀점
+//  배열의 합을 연산할때는 reduce를 생각하자. 그치만 2개의 배열의 루프를 돌릴때는 reduce가 for보다 속도가 느리므로 유의해서 쓰자.
 
 
