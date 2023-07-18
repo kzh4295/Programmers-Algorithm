7f372e1063b4bc76c50fe769d9e04c5635f8d34e
kzh4295 <kzh4295@ncits.co.kr>
2023-07-18 17:55:00 +0900

    k번째 자릿수 구하기

--- Lv1/kthNumber.js
+++ Lv1/kthNumber.js
@@ -1,16 +1,16 @@
 function solution(array, commands) {
-    let answer = []; 
+  let answer = [];
 
-    for(let i=0; i< commands.length; i++){
-      let arr = [];
-      arr.push(array.slice(commands[i][0]-1, commands[i][1]))
-  
-      let sortArr = arr.flat().sort()
-      answer.push(sortArr[commands[i][2]-1])
-    }
-
-    return answer;
+  for (let [i, j, k] of commands) {
+    answer.push(Number(
+      array
+        .slice(i - 1, j)
+        .sort((a, b) => a - b)[k - 1]
+    ));
+  }
+  return answer;
 }
+
 
 let result = solution([1, 5, 2, 6, 3, 7, 4],[[2, 5, 3], [4, 4, 1], [1, 7, 3]])
 
